4ce89486fb3460b9c67134027679c2bc8a0e36ef
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-07 11:30:03 +0000

    11CreateANewFunction

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,14 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+{
+if (total > 100) {
+   alert("We still have a bit of driving left to go");
+
+} else if (total > 50 && total <= 100) {
+  alert("We'll be there in 5 minutes");
+
+} else (total <= 50)
+    alert("I'm parking. I'll see your right now");
+}
+console.log(alert(60))
